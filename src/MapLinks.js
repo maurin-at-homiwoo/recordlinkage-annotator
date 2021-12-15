cd3bb6139bcc5fcdb4a8b38c28deadbd07ec26b2
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 <maurin@homiwoo.com>
2021-12-15 17:36:49 +0100

    Remove warning

--- src/MapLinks.js
+++ src/MapLinks.js
@@ -2,7 +2,6 @@ import React from 'react';
 import 'typeface-roboto';
 
 import Typography from '@material-ui/core/Typography';
-import ListItem from '@material-ui/core/ListItem';
 
 
 export default function MapLinks(props) {
